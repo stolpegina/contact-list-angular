f7566f1b6073b9494a7ff33df682445e85417568
Svetlana Tolpegina <stolpegina@gmail.com>
2022-02-28 16:57:35 +0400

    Added filter

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactModel } from '../../models/contact.model';
+import { CONTACTS } from './contacts.mockup';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.scss']
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit {
 
-  contacts: ContactModel[] = [
-    new ContactModel('Иванов Иван', '123456789'),
-    new ContactModel('Петров Алексей', '4656454654')
-  ];
+  contacts: ContactModel[] = [];
+
+  ngOnInit(): void {
+    this.contacts = CONTACTS;
+  }
 
   addContact(contact: ContactModel): void {
     this.contacts.push(contact);
+  }
+
+  filter(e: KeyboardEvent): void {
+    const value = (e.target as HTMLInputElement).value.toLocaleLowerCase();
+    if (value) {
+      this.contacts = this.contacts.filter(contact => contact.name.toLocaleLowerCase().includes(value));
+    } else {
+      this.contacts = CONTACTS;
+    }
+
   }
 }
---
+++ src/app/components/contact-list/contacts.mockup.ts
@@ -0,0 +1,6 @@
+import { ContactModel } from "src/app/models/contact.model";
+
+export const CONTACTS: ContactModel[] = [
+    new ContactModel('Иванов Иван', '123456789'),
+    new ContactModel('Петров Алексей', '4656454654')
+]
